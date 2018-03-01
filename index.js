c56e38b7fb66632b4700c004d217dba1c94fbd06
dicryptoid <dicryptoid@gmail.com>
2018-03-01 17:39:48 +0000

    added the Node Config module to the project

--- index.js
+++ index.js
@@ -2,37 +2,48 @@
  * @description the faucetman module's main entry point
  */
 // initialize all basic constants of the module
-const timer = require('./timer');
+const timer = require('./timer/index');
+const logger = require('./logger/index')('Faucetman');
+
+const config = require('config');
 const puppeteer = require('puppeteer');
 const url = require('url');
-console.log(`Faucetman: started at ${timer.started}`);
 
+logger.log(`started at ${new Date(timer.started).getUTCDate()}`);
 
 (async () => {
 	/** @todo: move this hardcore outside the module (somewhere to tasks config) */
-	const paths = {
-		target: url.parse('http://getfree.co.in/'),
-		screenshots: 'cache/screenshots/',
-		filename: '.png'
-	};
-	paths.filename = '' + paths.screenshots + paths.target.hostname + paths.filename;
-	console.log(`Faucetman: Initialized local variables and ready to GO! [+${timer.elapsed}ms.]`);
 
 	// initialize the browser and open new page
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
+	// configure the useragent properties
+	const viewport = config.get('app.useragent.viewport');
+	page.setViewport(viewport);
 
-	// navigate the page to a target URL
-	await page.goto(paths.target.href);
-	console.log(`Faucetman: launched the puppeteer's browser page at ${paths.target.href} [+${timer.elapsed}ms.]`);
+	// get the configuration for tasks and targets
+	const faucets = config.get('faucets');
+	const tasks = config.get('tasks');
 
-	// take the page screenshot
-	await page.screenshot({
-		path: paths.filename
-	});
-	console.log(`Faucetman: screenshot saved to ${paths.filename} [+${timer.elapsed}ms.]`);
+	logger.log('initialized local variables and ready to go', `+[${timer.elapsed}ms.]`);
+	do {
+		let task = tasks.shift(),
+			faucet = faucets[task.name],
+			location = url.parse(faucet.urls.home),
+			filename = `${config.get('app.dirs.screenshots')}${location.hostname}.png`;
+
+		// navigate the faucet homepage URL
+		logger.log(`fond the target page URL to go to: ${location.href}`, `+[${timer.elapsed}ms.]`);
+		await page.goto(location.href);
+		logger.log(`puppeteer ${location.href} page loaded`, `+[${timer.elapsed}ms.]`);
+
+		// take the homepage screenshot
+		await page.screenshot({ path: filename });
+		logger.log(`faucet's ${location.href} page screenshot saved to ${filename}`, `+[${timer.elapsed}ms.]`);
+
+	} while (tasks.length > 0);
 
 	// finally close the browser
 	await browser.close();
-	console.log(`Faucetman: finished and closed the browser; Took: ${timer.elapsed}ms.`);
+	logger.log('finished all tasks and released the browser', 'Total time:', timer.elapsed, 'ms.');
 })();
